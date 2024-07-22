eefe31c9bdb5570539cf7627a779697994181530
ridwanwiranata15 <jagokoding15@gmail.com>
2024-07-23 05:56:52 +0700

    modal create tugas selesai

--- js/homepage-teacher.js
+++ js/homepage-teacher.js
@@ -12,5 +12,38 @@ document.querySelector(".content-body-header select").addEventListener("change",
     } else {
         document.querySelector(".content-body-content-tugas").style.display = "none";
         document.querySelector(".content-body-content-materi").style.display = "block";
+   
     }
 });
+
+document.querySelector(".modal-create-tugas-content-footer button").addEventListener("click", function(e){
+    e.preventDefault();
+    const input  = document.querySelector(".create-tugas input");
+    const select = document.querySelector(".create-tugas select");
+    const date = document.querySelector(".create-tugas input[type='date']");
+    
+    let statusInput = false;
+
+
+    if(input.value.trim() == ""){
+        alert("form judul kosong")
+        statusInput = true;
+    }
+
+    if(date.value.trim() == ""){
+        alert("form tenggat waktu kosong")
+        statusInput = true;
+    }
+
+
+    if(statusInput){
+        alert("mohon isi terlebih dahulu");
+    }else{
+        alert("for sudah terisi");
+    }
+})
+
+document.querySelector(".modal-create-tugas-content-header span").addEventListener("click", function(e){
+    e.preventDefault();
+    document.querySelector(".modal-create-tugas").style.display = "none";
+})
